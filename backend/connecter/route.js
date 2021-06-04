3f75d7c673500647c7d2059a68bfdcaae70ed9cd
SANDIPAN <chak.sandipan22@gmail.com>
2021-06-04 22:14:15 +0530

    second stage

--- backend/connecter/route.js
+++ backend/connecter/route.js
@@ -1,24 +1,63 @@
 const express=require('express')
 const router=express.Router()
+const user=require('../model/user')
+
 
 router.get('/',(req,res)=>{
     res.send('homepage')
 })
 
-router.get('/about',(req,res)=>{
-    res.send('about me')
+router.post('/signup',midi1,(req,res)=>{
+    const  {name, email , password, phone }= req.body
+    user.findOne({email:email})
+    .then((yes)=>{
+        if(yes)
+        res.status(400).json({error: 'USER is already registered.'})
+        else{
+            const nuser= new user({
+                name:name,
+                email:email,
+                password:password,
+                phone:phone
+
+            })
+            nuser.save()
+            .then(()=> res.status(201).json({message:'SUCCESSFULLY REGISTERED'}))
+            .catch((err)=> res.status(402).json({error:'Unable to store'}))
+        }
+    })
+   
 })
 
-router.get('/contact',(req,res)=>{
-    res.send('contact me !')
+router.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    const u=await user.findOne({email:email})
+    if(u)
+    res.json(u)
+    res.json({error: "Wrong Cred"})
 })
 
-router.get("/login",(req,res)=>{
-    res.send('hello from login page')
-})
+function midi1(req,res,next){
+    const  {name, email , password, phone}= req.body
+     if (!name || !email  || !password || !phone)
+     {
+         res.status(422).json({error:'wrong format submitted!'})
+     }
+     else{
+         next();
+        
+     }
+ }
+// router.get('/contact',(req,res)=>{
+//     res.send('contact me !')
+// })
 
-router.get('/signup',(req,res)=>{
-    res.send('hello from signup page')
-})
+// router.get("/login",(req,res)=>{
+//     res.send('hello from login page')
+// })
+
+// router.get('/signup',(req,res)=>{
+//     res.send('hello from signup page')
+// })
 
 module.exports=router;
